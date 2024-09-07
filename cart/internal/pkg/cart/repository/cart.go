0547e64d305bde66748ecf808d78a1c7820b4931
Mikle95 <1mikle1@mail.ru>
2024-09-07 12:59:05 +0300

    Добавил mutex в работу с хранилищем

--- cart/internal/pkg/cart/repository/cart.go
+++ cart/internal/pkg/cart/repository/cart.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"sync"
 
 	"gitlab.ozon.dev/1mikle1/homework/cart/internal/pkg/cart/model"
 )
@@ -10,13 +11,17 @@ type Storage = map[model.Sku]model.CartItem
 
 type Cart struct {
 	storage Storage
+	mtx     sync.RWMutex
 }
 
 func NewCart() *Cart {
-	return &Cart{storage: make(Storage)}
+	return &Cart{storage: make(Storage), mtx: sync.RWMutex{}}
 }
 
 func (c *Cart) AddItem(_ context.Context, item model.CartItem) model.CartItem {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+
 	cartItem, exists := c.storage[item.SKU]
 	if exists {
 		item.Count += cartItem.Count
@@ -26,6 +31,9 @@ func (c *Cart) AddItem(_ context.Context, item model.CartItem) model.CartItem {
 }
 
 func (c *Cart) GetItems(_ context.Context) []model.CartItem {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+
 	out := make([]model.CartItem, 0, len(c.storage))
 	for _, val := range c.storage {
 		out = append(out, val)
@@ -34,6 +42,9 @@ func (c *Cart) GetItems(_ context.Context) []model.CartItem {
 }
 
 func (c *Cart) DeleteItem(_ context.Context, sku model.Sku) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+
 	_, exists := c.storage[sku]
 	if exists {
 		delete(c.storage, sku)
