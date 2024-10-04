c4277de83b53f6284711adc4122f979b53714fc1
Александров Михаил <aleksandrov_ma@citis.ru>
2024-10-04 14:55:37 +0300

    [GOHW-5]: Сделал отмену по контексту в product service, создал свою errgroup

---
+++ cart/internal/pkg/errorgroup/my_error_group.go
@@ -0,0 +1,42 @@
+package my_errorgroup
+
+import (
+	"context"
+	"sync"
+)
+
+type ErrorGroup struct {
+	cancel  func(error)
+	wg      sync.WaitGroup
+	errOnce sync.Once
+	err     error
+}
+
+func New(ctx context.Context) (*ErrorGroup, context.Context) {
+	ctx, cancel := context.WithCancelCause(ctx)
+	return &ErrorGroup{cancel: cancel}, ctx
+}
+
+func (g *ErrorGroup) Go(f func() error) {
+	g.wg.Add(1)
+	go func() {
+		defer g.wg.Done()
+
+		if err := f(); err != nil {
+			g.errOnce.Do(func() {
+				g.err = err
+				if g.cancel != nil {
+					g.cancel(g.err)
+				}
+			})
+		}
+	}()
+}
+
+func (g *ErrorGroup) Wait() error {
+	g.wg.Wait()
+	if g.cancel != nil {
+		g.cancel(g.err)
+	}
+	return g.err
+}
